3c5673288cb9cc81ef5d744aca024af66ee6efee
Natsuneco <135076690+Natsuneco@users.noreply.github.com>
2026-05-24 12:45:26 +0900

    Tailwindを再インポート

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vite.dev/config/
@@ -7,6 +8,7 @@ export default defineConfig({
   base: '/easy-scientific-calculator/',
   plugins: [
     react(),
+    tailwindcss(),
     VitePWA({
       registerType: 'autoUpdate',
       includeAssets: ['favicon.svg', 'apple-touch-icon.png', 'icons.svg'],
